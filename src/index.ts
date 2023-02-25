28ff3ce9600a08f4486b8c6eb4fa8b88771866ee
Saurmbhai <126186489+Saurmbhai@users.noreply.github.com>
2023-02-25 09:06:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,17 @@ import constants from "./constants";
 // CLI
 import * as cli from "./cli/ui";
 import { handleIncomingMessage } from "./handlers/message";
+//copied code for r10 port error
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 // Entrypoint
 const start = async () => {
 	cli.printIntro();
